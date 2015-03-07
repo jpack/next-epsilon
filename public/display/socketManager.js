2f1a8004cd0e41b4e70ef1e57e6e6154ad431485
Brian Lamb <brian.lamb@mdu.com>
2015-03-07 02:32:30 -0700

    Add more sounds. Update demo and sound engine

--- public/display/socketManager.js
+++ public/display/socketManager.js
@@ -1,4 +1,10 @@
 var socket = io();
+socket.on('add', function(tracks){
+    console.log(tracks);
+});
 socket.on('update', function(tracks){
+    console.log(tracks);
+});
+socket.on('delete', function(tracks){
     console.log(tracks);
 });
