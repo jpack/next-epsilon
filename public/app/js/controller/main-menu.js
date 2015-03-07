9e1983c557fb8de18cd557eca5dc27fa3fd39225
Brian Lamb <brian.lamb@mdu.com>
2015-03-07 08:21:13 -0700

    Prettify main-menu

--- public/app/js/controller/main-menu.js
+++ public/app/js/controller/main-menu.js
@@ -1,3 +1,31 @@
+
+
 nextEpsilon.controller('MainMenuCtrl', function ($scope) {
-    $scope.tracks = [1, 2, 3];
+    $scope.tracks = [1, 2, 3, 4, 5];
+
+    // 8 pitches for our interface
+    $scope.measure = new Array(8);
+
+
+    // 16 beats for each measure
+    for(var i = 0; i < 8; i++){
+        var notes = new Array(16);
+
+        for(var j = 0; j < 16; j++){
+            notes[j] = {active: false};
+        }
+
+        $scope.measure[i] = notes;
+    }
+
+    $scope.openSettings = function(obj) {
+        if (obj.open === undefined) {
+            obj.open = true;
+        }
+        else {
+            obj.open = !obj.open;
+        }
+        console.log("abc");
+    }
+
 });
