83fa9b94fc6d0b7ad1665c5411fa1dcd7384684e
Brian Lamb <brian.lamb@mdu.com>
2015-03-07 05:30:28 -0700

    Add splash screen

--- public/display/socketManager.js
+++ public/display/socketManager.js
@@ -1,10 +1,10 @@
 var socket = io();
-socket.on('add', function(tracks){
-    console.log(tracks);
+socket.on('add', function(track){
+    console.log(track);
 });
-socket.on('update', function(tracks){
-    console.log(tracks);
+socket.on('update', function(track){
+    console.log(track);
 });
-socket.on('delete', function(tracks){
+socket.on('delete', function(id){
     console.log(tracks);
 });
