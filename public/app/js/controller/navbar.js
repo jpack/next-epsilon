49c6de8e889b79973afa7e34292a833e1241ead6
Jayson Pack <jayson.pack@mdu.com>
2015-03-06 23:04:15 -0700

    Added sequencer save function.

--- public/app/js/controller/navbar.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by jayson.pack on 3/6/15.
- */
--- public/app/js/controller/sequencer.js
+++ public/app/js/controller/sequencer.js
@@ -1,4 +1,4 @@
-nextEpsilon.controller('SequencerCtrl', function ($scope) {
+nextEpsilon.controller('SequencerCtrl', function ($scope, $http) {
     // 8 pitches for our interface
     $scope.measure = new Array(8);
 
@@ -13,4 +13,34 @@ nextEpsilon.controller('SequencerCtrl', function ($scope) {
         $scope.measure[i] = notes;
     }
 
+    $scope.saveSequence = function(){
+        var trackTemplate = {
+            sampleId: 0,
+                isRecording: false,
+            offset: 0,
+            resolution: 1,
+            repeat: 2,
+            lock: false,
+            volume: 1,
+            notes: []
+        };
+
+        for(var i = 0; i < $scope.measure.length; i++){
+            for(var j = 0; j < $scope.measure[i].length; j++){
+                if($scope.measure[i][j].active){
+                    trackTemplate.notes.push({startTime: 2 * j, pitch: i});
+                }
+            }
+        }
+
+        $http.post('/addTrack', trackTemplate).
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+    }
 });
