c8a95fb563540599fecafcd3588b3856a573ab9b
Jayson Pack <jayson.pack@mdu.com>
2015-03-07 11:51:15 -0700

    App checkin

--- public/app/js/controller/sequencer.js
+++ public/app/js/controller/sequencer.js
@@ -1,22 +1,42 @@
 nextEpsilon.controller('SequencerCtrl', function ($scope, $http) {
     $scope.offset = 0;
-    $scope.measure = 0;
+    $scope.currMeasure = 0;
+    $scope.measures = [];
     $scope.repeat = false;
 
-    // 8 pitches for our interface
-    $scope.measures = [];
-    $scope.currMeasure = new Array(8);
+    var createMeasure = function(){
+        // 8 pitches per measure
+        var measure = new Array(8);
 
-    // 16 beats for each measure
-    for(var i = 0; i < 8; i++){
-        var notes = new Array(16);
+        for(var i = 0; i < 8; i++){
+            // 16 notes per measure
+            measure[i] = new Array(16);
 
-        for(var j = 0; j < 16; j++){
-            notes[j] = {active: false};
+            for(var j = 0; j < 16; j++){
+                measure[i][j] = {active: false};
+            }
         }
 
-        $scope.currMeasure[i] = notes;
-    }
+        return measure
+    };
+
+    $scope.addMeasure = function(){
+        $scope.currMeasure++;
+
+        if($scope.measures[$scope.currMeasure] == null){
+            $scope.measures[$scope.currMeasure] = createMeasure();
+        }
+    };
+
+    $scope.subMeasure = function(){
+        if($scope.currMeasure <= 0){
+            return;
+        }
+
+        $scope.currMeasure--;
+    };
+
+    $scope.measures[$scope.currMeasure] = createMeasure();
 
     $scope.saveSequence = function(){
         var trackTemplate = {
@@ -32,20 +52,12 @@ nextEpsilon.controller('SequencerCtrl', function ($scope, $http) {
 
         for(var i = 0; i < $scope.currMeasure.length; i++){
             for(var j = 0; j < $scope.currMeasure[i].length; j++){
-                if($scope.currMeasure[i][j].active){
+                if($scope.currMeasure[i][j]){
                     trackTemplate.notes.push({startTime: 2 * j, pitch: i});
                 }
             }
         }
 
-        $http.post('/addTrack', trackTemplate).
-            success(function(data, status, headers, config) {
-                // this callback will be called asynchronously
-                // when the response is available
-            }).
-            error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
+        $http.post('/addTrack', trackTemplate);
     }
 });
