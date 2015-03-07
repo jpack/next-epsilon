a139fb03a6a8f65061e0809519b4d2517e87070c
Anthony Quarnstrom <Quarnstroma@gmail.com>
2015-03-07 02:05:15 +0000

    server.js edited online with Bitbucket

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-// server.js
-
-// set up ========================
 var express = require('express');
-var app = express(); // create our app w/ express
-var server = require('http').Server(app);
+var app = express();
+var server = app.listen(3000);
 var io = require('socket.io')(server);
+
 
 // configuration =================
 app.use(express.static('public'));
 
 app.get('/app', function(req, res) {
-	res.sendfile('./public/app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendfile('./public/app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 app.get('/display', function(req, res) {
     res.sendfile('./public/display/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
-server.listen(3000);
 console.log("App listening on port 3000");
+//Added code begins below:
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
