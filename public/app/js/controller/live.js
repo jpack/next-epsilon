02a63985d7a61e67ca8c6f4542dfe420906ea52f
Jayson Pack <jayson.pack@mdu.com>
2015-03-07 14:46:55 -0700

    Basic live interface.

--- public/app/js/controller/live.js
+++ public/app/js/controller/live.js
@@ -1,6 +1,5 @@
 nextEpsilon.controller('LiveCtrl', function ($scope) {
     $scope.sampleId = 0;
-    var socket = io();
 
     $scope.play = function(pitch){
         socket.emit('noteSend', {sampleId: $scope.sampleId, pitch: pitch})
